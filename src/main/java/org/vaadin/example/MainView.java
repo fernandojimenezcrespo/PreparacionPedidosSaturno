aaa596aac12fd73da1fc0c327e9b9e59b4a20007
fernandojimenezcrespo <73723133+fernandojimenezcrespo@users.noreply.github.com>
2022-01-03 12:43:32 +0100

    Creacíon del proyecto PreparacionPedidosSaturno

---
+++ src/main/java/org/vaadin/example/MainView.java
@@ -0,0 +1,53 @@
+package org.vaadin.example;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+import org.springframework.beans.factory.annotation.Autowired;
+ 
+@Route
+@PWA(name = "Vaadin Application",
+        shortName = "Vaadin App",
+        description = "This is an example Vaadin application.",
+        enableInstallPrompt = false)
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class MainView extends HorizontalLayout {
+
+    public MainView(@Autowired GreetService service) {
+
+        // Use TextField for standard text input
+        VerticalLayout vertical_izq = new VerticalLayout();
+        VerticalLayout vertical_dcha= new VerticalLayout();
+        TextField textField = new TextField("Your name");
+        TextField nif = new TextField("Nif");
+        textField.addThemeName("bordered");
+        nif.addThemeName("bordered");
+
+        // Button click listeners can be defined as lambda expressions
+        Button button = new Button("Say hello",
+                e -> Notification.show(service.greet(textField.getValue())));
+
+        // Theme variants give you predefined extra styles for components.
+        // Example: Primary button has a more prominent look.
+        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        // You can specify keyboard shortcuts for buttons.
+        // Example: Pressing enter in this view clicks the Button.
+        button.addClickShortcut(Key.ENTER);
+
+        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
+        addClassName("centered-content");
+        vertical_izq.add(textField,button);
+        vertical_dcha.add(nif);
+        add(vertical_izq,vertical_dcha);
+    }
+
+}
