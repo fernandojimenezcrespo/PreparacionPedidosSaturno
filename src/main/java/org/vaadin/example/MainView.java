f1e2702e235a3d0a3c7a6403d96a74e331c44c20
fernandojimenezcrespo <73723133+fernandojimenezcrespo@users.noreply.github.com>
2022-01-03 13:30:46 +0100

    combos

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -3,6 +3,7 @@ package org.vaadin.example;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -11,7 +12,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import org.springframework.beans.factory.annotation.Autowired;
- 
+
 @Route
 @PWA(name = "Vaadin Application",
         shortName = "Vaadin App",
@@ -25,15 +26,23 @@ public class MainView extends HorizontalLayout {
 
         // Use TextField for standard text input
         VerticalLayout vertical_izq = new VerticalLayout();
-        VerticalLayout vertical_dcha= new VerticalLayout();
-        TextField textField = new TextField("Your name");
-        TextField nif = new TextField("Nif");
-        textField.addThemeName("bordered");
-        nif.addThemeName("bordered");
+        VerticalLayout vertical_dcha = new VerticalLayout();
+        TextField textCampoNombre = new TextField("Your name");
+        ComboBox<String> cmbTipoPedido = new ComboBox();
+        ComboBox<String> cmbServicioSeccion = new ComboBox();
+        textCampoNombre.addThemeName("bordered");
+        cmbTipoPedido.setId("idTipoPedido");
+        cmbTipoPedido.setLabel("Tipo de Pedido");
+        cmbTipoPedido.setItems("Almacen","Compra Directa");
+        cmbServicioSeccion.setId("idServicioSeccion");
+        cmbServicioSeccion.setLabel("Servicio/Seccion");
+        cmbServicioSeccion.setItems("Lab./Hema","Lab./Urg.","Lab./Micro");
+        
+        textCampoNombre.setId("idCampoNombre");
 
         // Button click listeners can be defined as lambda expressions
         Button button = new Button("Say hello",
-                e -> Notification.show(service.greet(textField.getValue())));
+                e -> Notification.show(service.greet(textCampoNombre.getValue())));
 
         // Theme variants give you predefined extra styles for components.
         // Example: Primary button has a more prominent look.
@@ -45,9 +54,9 @@ public class MainView extends HorizontalLayout {
 
         // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
         addClassName("centered-content");
-        vertical_izq.add(textField,button);
-        vertical_dcha.add(nif);
-        add(vertical_izq,vertical_dcha);
+        vertical_izq.add(textCampoNombre, button);
+        vertical_dcha.add(cmbTipoPedido,cmbServicioSeccion);
+        add(vertical_izq, vertical_dcha);
     }
 
 }
