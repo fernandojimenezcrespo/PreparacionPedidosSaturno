0ba96f15d55b8da64990510d5eed9f604190feeb
fernandojimenezcrespo <73723133+fernandojimenezcrespo@users.noreply.github.com>
2022-01-05 19:48:37 +0100

    Por hoy Satisfecho

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -7,12 +7,14 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.shared.Registration;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,14 +30,19 @@ import org.vaadin.utils.LeerFicherosXML;
         enableInstallPrompt = false)
 //@CssImport("./styles/shared-styles.css")
 //@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-public class MainView extends VerticalLayout {
+public class MainView extends HorizontalLayout {
+
+    boolean hayTipo = false;
+    boolean hayServicio = false;
+    Articulos articulo = new Articulos();
+    List<Articulos> articulosList = new ArrayList<Articulos>();
+    Grid<Articulos> gridArticulos = new Grid<>(Articulos.class);
 
     public MainView(@Autowired GreetService service) {
-
-        // Use TextField for standard text input
         VerticalLayout vertical_izq = new VerticalLayout();
         VerticalLayout vertical_dcha = new VerticalLayout();
-        vertical_dcha.setWidthFull();
+        vertical_dcha.setWidth("80%");
+        vertical_izq.setWidth("20%");
         TextField textCampoNombre = new TextField("Your name");
         ComboBox<String> cmbTipoPedido = new ComboBox();
         ComboBox<String> cmbServicioSeccion = new ComboBox();
@@ -46,17 +53,47 @@ public class MainView extends VerticalLayout {
         cmbServicioSeccion.setId("idServicioSeccion");
         cmbServicioSeccion.setLabel("Servicio/Seccion");
         cmbServicioSeccion.setItems("Lab./Hema", "Lab./Urg.", "Lab./Micro");
-        Articulos articulo = new Articulos();
-        List<Articulos> articulosList = new ArrayList<Articulos>();
-        articulosList = cargaArticulos(articulo, articulosList);
-        Grid<Articulos> grid = new Grid<>(Articulos.class);
-         
-        grid.setAllRowsVisible(false);
+
+        Span spanAclaracionTipo = new Span("-");
+        Span spanAclaracionServicioSeccion = new Span("-");
+        /*cmbTipoPedido.addValueChangeListener(event
+                -> spanAclaracionTipo.setText("El tipo es: " + event.getValue())
+                );*/
+ /*cmbServicioSeccion.addValueChangeListener(event
+                -> spanAclaracionServicioSeccion.setText("El Servicio es: " + event.getValue()));*/
+        cmbTipoPedido.addValueChangeListener(event
+                -> {
+            spanAclaracionTipo.setText("El tipo es: " + event.getValue());
+            hayTipo = actulizaTipoPedido(event.getValue());
+            if (hayTipo && hayServicio) {
+                gridArticulos.setVisible(true);
+                articulosList = cargaArticulos(articulo, articulosList);
+                gridArticulos.setItems(articulosList);
+            }
+
+        }
+        );
+        cmbServicioSeccion.addValueChangeListener(event
+                -> {
+            spanAclaracionServicioSeccion.setText("El tipo es: " + event.getValue());
+            hayServicio = actulizaServicioSeccion(event.getValue());
+            if (hayTipo && hayServicio) {
+                gridArticulos.setVisible(true);
+                articulosList = cargaArticulos(articulo, articulosList);
+                gridArticulos.setItems(articulosList);
+            }
+        }
+        );
+        if (hayTipo && hayServicio) {
+            articulosList = cargaArticulos(articulo, articulosList);
+        }
+
+        gridArticulos.setVisible(false);
+
         //grid.setWidthFull();
-        
         //grid.setSizeFull();
-        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);//No pone bordes verticales.
-         grid.setItems(articulosList);
+        gridArticulos.addThemeVariants(GridVariant.LUMO_NO_BORDER);//No pone bordes verticales.
+        
 
         textCampoNombre.setId("idCampoNombre");
 
@@ -74,9 +111,9 @@ public class MainView extends VerticalLayout {
 
         // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
         addClassName("centered-content");
-        vertical_izq.add(textCampoNombre, button);
-        vertical_dcha.add(grid,cmbTipoPedido, cmbServicioSeccion);
-        add( vertical_dcha);
+        vertical_izq.add(textCampoNombre, button, spanAclaracionTipo, spanAclaracionServicioSeccion);
+        vertical_dcha.add(cmbTipoPedido, cmbServicioSeccion, gridArticulos);
+        add(vertical_izq, vertical_dcha);
     }
 
     private List<Articulos> cargaArticulos(Articulos articulo, List<Articulos> articulosList) {
@@ -94,5 +131,21 @@ public class MainView extends VerticalLayout {
         }
         return null;
 
+    }
+
+    private boolean actulizaTipoPedido(String value) {
+        if (!value.isEmpty() && !value.trim().equals("")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean actulizaServicioSeccion(String value) {
+        if (!value.isEmpty() && !value.trim().equals("")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
