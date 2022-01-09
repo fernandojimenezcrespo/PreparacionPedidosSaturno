05e0ed5882c6b1991eb68ff54cb3b54e36294eab
fernandojimenezcrespo <73723133+fernandojimenezcrespo@users.noreply.github.com>
2022-01-09 18:39:30 +0100

    Empezando PDF

--- src/main/java/org/vaadin/example/GreetService.java
+++ src/main/java/org/vaadin/example/GreetService.java
@@ -1,18 +1,22 @@
 package org.vaadin.example;
 
+import com.itextpdf.text.DocumentException;
+import java.io.FileNotFoundException;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.stereotype.Service;
+import org.vaadin.pdf.GenerarPdf;
 
 @Service
 public class GreetService implements Serializable {
 
-    public String greet(String name) {
+    public String greet(String name) throws FileNotFoundException {
         if (name == null || name.isEmpty()) {
-            return "Hello anonymous user";
+            return "En busca de generar....PDF";
         } else {
             return "Hello " + name;
         }
     }
-
 }
